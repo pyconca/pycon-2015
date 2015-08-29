0b0d821e53a25c490e2f24e2ea0260f9ad441264
David Hoffman <dhoffman34@icloud.com>
2015-08-29 10:05:08 -0400

    updated SponsorView to sort sponsors

--- pycon/sponsors/enums.py
+++ pycon/sponsors/enums.py
@@ -2,10 +2,10 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class SponsorLevels():
-    DIAMOND = '1'
-    GOLD = '2'
-    SILVER = '3'
-    BRONZE = '4'
+    DIAMOND = 'diamond'
+    GOLD = 'gold'
+    SILVER = 'silver'
+    BRONZE = 'bronze'
 
     choices = (
         (DIAMOND, _('Diamond')),
--- pycon/sponsors/views.py
+++ pycon/sponsors/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pycon.core.views import PyconTemplateView
 from pycon.sponsors.models import Sponsor
 
@@ -6,6 +7,9 @@ class SponsorView(PyconTemplateView):
     template_name = 'sponsor.html'
 
     def get(self, request):
+        sponsors = defaultdict(list)
+        for sponsor in Sponsor.objects.all():
+            sponsors[sponsor.level].append(sponsor)
         return self.render_to_response({
-            'sponsors': Sponsor.objects.all().order_by('level'),
+            'sponsors': sponsors,
         })
