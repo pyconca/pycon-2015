6249511ed74212b5b57d64e107d6deb4ccecb450
David Hoffman <dhoffman34@icloud.com>
2015-08-29 09:41:22 -0400

    added twitter and i18n to Sponsor model

--- pycon/sponsors/models.py
+++ pycon/sponsors/models.py
@@ -9,9 +9,12 @@ class Sponsor(models.Model):
     def upload_path(self, filename):
         return os.path.basename(filename)
 
-    name = models.CharField(max_length=128)
-    description = models.TextField(max_length=512)
+    name_en = models.CharField(max_length=128)
+    name_fr = models.CharField(max_length=128)
+    description_en = models.TextField(max_length=512)
+    description_fr = models.TextField(max_length=512)
     logo = models.ImageField(max_length=512, upload_to=upload_path)
-    logo_alt = models.ImageField(max_length=512, upload_to=upload_path)
     level = models.CharField(max_length=64, choices=SponsorLevels.choices)
-    link = models.URLField(blank=True)
+    url_twitter = models.URLField(blank=True)
+    url_website = models.URLField(blank=True)
+
