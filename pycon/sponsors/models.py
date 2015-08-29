a07612b9228098bf0a3eb297cc75b9f0eb5bd1d0
David Hoffman <dhoffman34@icloud.com>
2015-08-29 09:47:32 -0400

    made remaining Sponsor fields i18n

--- pycon/sponsors/models.py
+++ pycon/sponsors/models.py
@@ -9,12 +9,15 @@ class Sponsor(models.Model):
     def upload_path(self, filename):
         return os.path.basename(filename)
 
-    name_en = models.CharField(max_length=128)
-    name_fr = models.CharField(max_length=128)
     description_en = models.TextField(max_length=512)
     description_fr = models.TextField(max_length=512)
-    logo = models.ImageField(max_length=512, upload_to=upload_path)
+    logo_en = models.ImageField(max_length=512, upload_to=upload_path)
+    logo_fr = models.ImageField(max_length=512, upload_to=upload_path)
+    name_en = models.CharField(max_length=128)
+    name_fr = models.CharField(max_length=128)
     level = models.CharField(max_length=64, choices=SponsorLevels.choices)
-    url_twitter = models.URLField(blank=True)
-    url_website = models.URLField(blank=True)
+    url_twitter_en = models.URLField(blank=True)
+    url_twitter_fr = models.URLField(blank=True)
+    url_website_en = models.URLField(blank=True)
+    url_website_fr = models.URLField(blank=True)
 
