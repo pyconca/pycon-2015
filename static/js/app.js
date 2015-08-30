61fbb5a9adea54f1c5a7281ecc5120f30e86f3fb
quaspas <daniel.quattro@gmail.com>
2015-08-29 22:02:13 -0400

    Mobile menu and language switch.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,10 @@
 $(function () {
+    //mobile menu
+    $("a[href=#menu]").click(function (e) {
+        $(".menu").toggleClass("menu-open");
+        e.preventDefault();
+    });
+
     // messages
     $('.message .icon-cancel').click(function () {
         $(this).parent().slideUp(200);
