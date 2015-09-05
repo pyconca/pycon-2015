b93eb137a622cf76f627003836e3bb4a62c6090d
quaspas <daniel.quattro@gmail.com>
2015-09-05 13:24:59 -0400

    Remove mailing app.
    We will be using mailchimp directly.

--- pycon/core/views.py
+++ pycon/core/views.py
@@ -1,12 +1,8 @@
 from django.views.generic.base import TemplateView
 
-from pycon.mailing.forms import MailingSignUpForm
-
 
 class PyconTemplateView(TemplateView):
     def render_to_response(self, context, **response_kwargs):
-        if context.get('mailing_form') is None:
-            context['mailing_form'] = MailingSignUpForm()
         response_kwargs.setdefault('content_type', self.content_type)
         return self.response_class(
             request=self.request,
