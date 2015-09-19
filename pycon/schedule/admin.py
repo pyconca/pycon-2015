293c283c88bb4f1bff9e7771380bbc29fb2afef2
Myles Braithwaite <me@mylesbraithwaite.com>
2015-09-18 20:04:19 -0400

    Started working on the Schedule page.

--- pycon/schedule/admin.py
+++ pycon/schedule/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from pycon.schedule.models import *
 
-# Register your models here.
+
+class SlotRoomInline(admin.TabularInline):
+    model = SlotRoom
+    extra = 1
+
+
+class SlotAdmin(admin.ModelAdmin):
+    list_filter = ("day", "kind")
+    list_display = ("day", "start", "end", "kind")
+    inlines = [SlotRoomInline]
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ["name", "order"]
+    inlines = [SlotRoomInline]
+
+
+class PresentationAdmin(admin.ModelAdmin):
+    model = Presentation
+    list_filter = ("cancelled", "slot")
+
+
+admin.site.register(Day)
+admin.site.register(
+    SlotKind,
+    list_display=["label"],
+)
+admin.site.register(
+    SlotRoom,
+    list_display=["slot", "room"]
+)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Slot, SlotAdmin)
+admin.site.register(Session)
+admin.site.register(Presentation, PresentationAdmin)
+admin.site.register(Speaker)
--- pycon/schedule/urls.py
+++ pycon/schedule/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from pycon.schedule.views import ScheduleView
+from pycon.schedule.views import ScheduleView, TalkDetailView
 
 urlpatterns = [
+    url(r"^(?P<pk>[0-9]+)/$", TalkDetailView.as_view(), name='schedule_talk_detail'),
     url(r"^$", ScheduleView.as_view(), name='schedule'),
 ]
