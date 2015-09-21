4ddb1210660257efe70419053511893cf360d9c8
Myles Braithwaite <me@mylesbraithwaite.com>
2015-09-21 15:42:16 -0400

    Disabling the presentation detail page as it doesn't work and I don't know why.

--- pycon/schedule/urls.py
+++ pycon/schedule/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from pycon.schedule.views import ScheduleView, PresentationView
 
 urlpatterns = [
-    url(r"^(?P<pk>[0-9]+)/$", PresentationView.as_view(), name='schedule_talk_detail'),
+    # url(r"^(?P<pk>[0-9]+)/$", PresentationView.as_view(), name='schedule_talk_detail'),
     url(r"^$", ScheduleView.as_view(), name='schedule'),
 ]
