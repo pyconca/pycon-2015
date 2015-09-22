4d47d96076612af8cb49a9ce82951769f59cee48
Myles Braithwaite <me@mylesbraithwaite.com>
2015-09-22 15:55:21 -0400

    Fixed the issue with the view and urls.

--- pycon/schedule/urls.py
+++ pycon/schedule/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 from pycon.schedule.views import ScheduleView, PresentationView
 
 urlpatterns = [
-    # url(r"^(?P<pk>[0-9]+)/$", PresentationView.as_view(), name='schedule_talk_detail'),
-    url(r"^$", ScheduleView.as_view(), name='schedule'),
+    url(
+        regex = r"^$",
+        view = ScheduleView.as_view(),
+        name = 'schedule'
+    ),
+    url(
+        regex = r"^(?P<pk>[0-9]+)/$",
+        view = PresentationView.as_view(),
+        name = 'schedule_presentation_detail'
+    ),
 ]
