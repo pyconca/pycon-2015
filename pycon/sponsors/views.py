f2fc13df6dbcbc81bb5a58ce24dfe2eb08d8fa84
quaspas <daniel.quattro@gmail.com>
2015-09-24 11:50:45 -0400

    Added order within sponsor levels.

--- pycon/sponsors/views.py
+++ pycon/sponsors/views.py
@@ -7,11 +7,12 @@ class SponsorView(PyconTemplateView):
 
     def get(self, request):
         sponsors_list = []
-        sponsors = Sponsor.objects.filter(type__isnull=False).order_by('type__order').select_related('type')
+        sponsors = Sponsor.objects.filter(type__isnull=False).order_by('type__order', 'order').select_related('type')
         for sponsor in sponsors:
             for sponsor_list in sponsors_list:
-                if sponsor.type.id == sponsor_list[0].id:
+                if sponsor.type.id == sponsor_list[0].type.id:
                     sponsor_list.append(sponsor)
+                    break
             else:
                 sponsors_list.append([sponsor])
         return self.render_to_response({
