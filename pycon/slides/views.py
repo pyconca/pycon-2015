a8b6f6c67e666603d90a413840a3d8048016bd1b
David Hoffman <dhoffman34@icloud.com>
2015-10-14 21:40:30 -0400

    added basic slide render

--- pycon/slides/views.py
+++ pycon/slides/views.py
@@ -1,5 +1,18 @@
 from django.http.response import HttpResponse
+from django.template import loader
+from pycon.schedule.models import Day
+from pycon.schedule.timetable import TimeTable
 
 
 def slides(request):
+    for day in Day.objects.all():
+        timetable = TimeTable(day)
+        prev_row = None
+        for next_row in timetable:
+            context = {
+                'prev_row': prev_row,
+                'next_row': next_row
+            }
+            print(loader.render_to_string('slides/slide.html', context))
+            prev_row = next_row
     return HttpResponse()
