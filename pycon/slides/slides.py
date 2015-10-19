8f02b18739b68b62e4aafac5db40c6b9b8d55a56
David Hoffman <dhoffman34@icloud.com>
2015-10-18 20:46:49 -0400

    added previous slide text

--- pycon/slides/slides.py
+++ pycon/slides/slides.py
@@ -1,4 +1,21 @@
-from pycon.schedule.models import Day, Room, Slot
+from pycon.schedule.models import Room, Slot
+
+
+def gen_prev_text(slot):
+    text = ""
+
+    if getattr(slot, 'kind_label', None) is None:
+        return ''
+
+    if slot.kind_label == 'talk' and slot.presenter:
+        text = "Thanks for viewing {}'s talk!".format(slot.presenter)
+    elif slot.kind_label == 'tutorial' and slot.presenter:
+        text = "Thanks for attending {}'s tutorial!".format(slot.presenter)
+    else:
+        text = slot.content_override
+    if slot.feedback_url:
+        text += " Please give feedback at {}.".format(slot.feedback_url)
+    return text
 
 
 class Slides(object):
@@ -12,6 +29,7 @@ class Slides(object):
                 result = {
                     'prev_start': getattr(prev_slot, 'start', None),
                     'prev_end': getattr(prev_slot, 'end', None),
+                    'prev_text': gen_prev_text(prev_slot),
                     'next_start': getattr(next_slot, 'start', None),
                     'next_end': getattr(next_slot, 'end', None),
                 }
