f0d22f27cbda561f93e593d7503d6d30df1f0e8d
David Hoffman <dhoffman34@icloud.com>
2015-10-22 21:00:12 -0400

    added png conversion

--- pycon/slides/views.py
+++ pycon/slides/views.py
@@ -1,27 +1,52 @@
-import errno
 import os
 import shutil
+import subprocess
 from uuid import uuid4
+from django.conf import settings
 from django.http.response import HttpResponse
 from django.template import loader
+from pycon.schedule.models import Room
 from pycon.slides.slides import Slides
 
 
 def slides(request):
     base_dir = '/tmp/slides.' + uuid4().hex
     html_dir = os.path.join(base_dir, 'html')
-    os.makedirs(html_dir)
-    for slide in Slides():
-        room_dir = os.path.join(html_dir, slide['room'].name)
-        try:
-            os.makedirs(room_dir)
-        except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                raise
-        slide_path = os.path.join(room_dir, slide['next_start'].strftime('%H%M%S')) + '.html'
-        html = loader.render_to_string('slides/slide.html', slide)
-        slide_file = open(slide_path, 'w')
-        slide_file.write(html)
-        slide_file.close()
-    shutil.rmtree(base_dir)
+    png_dir = os.path.join(base_dir, 'png')
+    try:
+        os.makedirs(html_dir)
+        html_paths = []
+        for room in Room.objects.all():
+            os.makedirs(os.path.join(html_dir, room.name))
+            os.makedirs(os.path.join(png_dir, room.name))
+
+        for slide in Slides():
+            html_room_dir = os.path.join(html_dir, slide['room'].name)
+            html_path = os.path.join(html_room_dir, slide['next_start'].strftime('%H%M%S')) + '.html'
+            html = loader.render_to_string('slides/slide.html', slide)
+            html_file = open(html_path, 'w')
+            html_file.write(html)
+            html_file.close()
+            html_paths.append(html_path)
+
+        for html_path in html_paths:
+            png_path = html_path.replace('/html/', '/png/')\
+                                .replace('.html', '.png')
+            png_dir, png_name = os.path.split(png_path)
+            subprocess.check_call([
+                settings.WEBKIT2PNG_PATH,
+                '-F',  # just full size image
+                '-W',
+                str(settings.SLIDE_WIDTH),
+                '-H',
+                str(settings.SLIDE_HEIGHT),
+                '-D',
+                png_dir,
+                '-o',
+                png_name,
+                html_path,
+            ])
+
+    finally:
+        shutil.rmtree(base_dir)
     return HttpResponse()
