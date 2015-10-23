b9340afaba2ce45596a72b4349a409691bb075b8
David Hoffman <dhoffman34@icloud.com>
2015-10-22 23:02:01 -0400

    added date to slides paths

--- pycon/slides/views.py
+++ pycon/slides/views.py
@@ -7,13 +7,16 @@ from django.conf import settings
 from django.http.response import HttpResponse
 from django.shortcuts import redirect
 from django.template import loader
-from pycon.schedule.models import Room
 from pycon.slides.slides import Slides
 
 
 def slides(request):
     def generate_html(slide):
-        html_room_dir = os.path.join(html_dir, slide['room'].name)
+        html_room_dir = os.path.join(html_dir, slide['room'].name, str(slide['day'].date))
+        try:
+            os.makedirs(html_room_dir)
+        except FileExistsError:
+            pass
         html_path = os.path.join(html_room_dir, slide['next_start'].strftime('%H%M%S')) + '.html'
         html = loader.render_to_string('slides/slide.html', slide)
         html_file = open(html_path, 'w')
@@ -42,27 +45,25 @@ def slides(request):
 
     base_dir = '/tmp/slides.' + uuid4().hex
     html_dir = os.path.join(base_dir, 'html')
-    png_dir = os.path.join(base_dir, 'png')
 
     try:
         os.makedirs(html_dir)
         html_paths = []
-        for room in Room.objects.all():
-            os.makedirs(os.path.join(html_dir, room.name))
-            os.makedirs(os.path.join(png_dir, room.name))
 
         for slide in Slides():
+            print(str(slide['next_start']) + slide['room'].name)
             generate_html(slide)
 
         png_paths = []
         for html_path in html_paths:
             png_paths.append(html_to_png(html_path))
+        print(png_paths)
 
         zip_path = os.path.join(settings.MEDIA_ROOT, 'slides.zip')
         zip = zipfile.ZipFile(zip_path, 'w')
         for path in png_paths:
-            dir, filename = os.path.split(path)
-            zip.write(path, os.path.join(os.path.basename(dir), filename))
+            rel_path = '/'.join(path.split('/')[-3:])
+            zip.write(path, rel_path)
         response = HttpResponse(zip_path, content_type='application/zip')
         response['Content-Disposition'] = 'attachment; filename="slides.zip"'
     finally:
